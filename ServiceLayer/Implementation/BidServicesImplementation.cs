9852415593d4ba019200f92f1e311afa7872a329
MateiDebu <102983526+MateiDebu@users.noreply.github.com>
2024-08-27 10:55:39 +0300

    Implemented the methods from BidServicesImplementation.

--- ServiceLayer/Implementation/BidServicesImplementation.cs
+++ ServiceLayer/Implementation/BidServicesImplementation.cs
@@ -2,45 +2,115 @@
 using DomainModel.Models;
 using log4net;
 using ServiceLayer.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceLayer.Implementation
 {
+    /// <summary>
+    /// The bid services.
+    /// </summary>
+    /// <seealso cref="ServiceLayer.Interfaces.IBidServices" />
     public class BidServicesImplementation : IBidServices
     {
+        /// <summary>
+        /// The logger
+        /// </summary>
         private ILog logger = LogManager.GetLogger(typeof(UserServicesImplementation));
+        /// <summary>
+        /// The bid data services
+        /// </summary>
         private IBidDataServices bidDataServices;
+        /// <summary>
+        /// The user score and limits data services
+        /// </summary>
         private IUserScoreAndLimitsDataServices userScoreAndLimitsDataServices;
 
-        public BidServicesImplementation(ILog logger, IBidDataServices bidDataServices, IUserScoreAndLimitsDataServices userScoreAndLimitsDataServices)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BidServicesImplementation"/> class.
+        /// </summary>
+        /// <param name="bidDataServices">The bid data services.</param>
+        /// <param name="userScoreAndLimitsDataServices">The user score and limits data services.</param>
+        public BidServicesImplementation(IBidDataServices bidDataServices, IUserScoreAndLimitsDataServices userScoreAndLimitsDataServices)
         {
-            this.logger = logger;
             this.bidDataServices = bidDataServices;
             this.userScoreAndLimitsDataServices = userScoreAndLimitsDataServices;
         }
 
+        /// <summary>
+        /// Adds the bid.
+        /// </summary>
+        /// <param name="bid">The bid.</param>
+        /// <returns></returns>
         public bool AddBid(Bid bid)
         {
-            throw new NotImplementedException();
+            if (bid == null)
+            {
+                this.logger.Warn("Attempted to add a null bid.");
+                return false;
+            }
+
+            var context = new ValidationContext(bid, serviceProvider: null, items: null);
+            var results = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(bid, context, results, true))
+            {
+                this.logger.Warn("Attempted to add an invalid bid." + String.Join(' ', results));
+                return false;
+            }
+
+            if (this.bidDataServices.GetNoOfActiveBidsByUserId(bid.Buyer.Id) >= this.userScoreAndLimitsDataServices.GetUserLimitsByUserId(bid.Buyer.Id))
+            {
+                this.logger.Warn("Attempted to bid over limit.");
+            }
+
+            if (bid.DateAndTime < bid.Product.StartDate || bid.DateAndTime > bid.Product.EndDate || bid.DateAndTime > bid.Product.TerminationDate)
+            {
+                this.logger.Warn("Attempted to bid before the auction started or after the auction ended.");
+                return false;
+            }
+
+            if(bid.Currency != bid.Product.Currency)
+            {
+                this.logger.Warn("Attempted to bid with different currency.");
+                return false;
+            }
+
+            var existingBids = this.bidDataServices.GetBidsByProductId(bid.Product.Id);
+            if((existingBids.Count == 0 && bid.Amount < bid.Product.StartingPrice) || (existingBids.Count > 0 && (bid.Amount < existingBids[0].Amount || bid.Buyer.Id == existingBids[0].Buyer.Id)))
+            {
+                this.logger.Warn("Attempted to bid with not enough money or user already has the winning bid.");
+                return false;
+            }
+
+            return this.bidDataServices.AddBid(bid);
         }
 
+        /// <summary>
+        /// Gets all bids.
+        /// </summary>
+        /// <returns></returns>
         public IList<Bid> GetAllBids()
         {
-            throw new NotImplementedException();
+            return this.bidDataServices.GetAllBids();
         }
 
+        /// <summary>
+        /// Gets the bid by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
         public Bid GetBidById(int id)
         {
-            throw new NotImplementedException();
+            return this.bidDataServices.GetBidById(id);
         }
 
+        /// <summary>
+        /// Gets the bids by product identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
         public IList<Bid> GetBidsByProductId(int id)
         {
-            throw new NotImplementedException();
+            return this.bidDataServices.GetBidsByProductId(id);
         }
     }
 }
