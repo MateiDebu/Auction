e4908d3bc20dfb937d418a6bd10feb284353d941
MateiDebu <102983526+MateiDebu@users.noreply.github.com>
2024-08-30 13:24:23 +0300

    log4net settings.

--- DataMapper/AssemblyInfo.cs
+++ DataMapper/AssemblyInfo.cs
@@ -2,9 +2,10 @@
 // Debu Matei
 // </copyright>
 
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("8ca0bd08-15fc-4a80-95fd-f321647de5d3")]
+// Configure log4net using the .config file
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+// This will cause log4net to look for a configuration file
+// called TestApp.exe.config in the application base
+// directory (i.e. the directory containing TestApp.exe)
+// The config file will be watched for changes.
--- ServiceLayer/AssemblyInfo.cs
+++ ServiceLayer/AssemblyInfo.cs
@@ -2,19 +2,10 @@
 // Debu Matei
 // </copyright>
 
-using System.Runtime.InteropServices;
-
-// In SDK-style projects such as this one, several assembly attributes that were historically
-// defined in this file are now automatically added during build and populated with
-// values defined in project properties. For details of which attributes are included
-// and how to customise this process see: https://aka.ms/assembly-info-properties
-
-// Setting ComVisible to false makes the types in this assembly not visible to COM
-// components.  If you need to access a type in this assembly from COM, set the ComVisible
-// attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM.
-[assembly: Guid("8ca0bd08-15fc-4a80-95fd-f321647de5d3")]
-
+// Configure log4net using the .config file
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+// This will cause log4net to look for a configuration file
+// called TestApp.exe.config in the application base
+// directory (i.e. the directory containing TestApp.exe)
+// The config file will be watched for changes.
